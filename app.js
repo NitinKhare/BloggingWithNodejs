9f46f7cb7d8f874db5579046d0c33c860c4aea12
Nitin <nitinkhare047@gmail.com>
2018-08-07 00:22:41 +0530

    Added Create Blog route

--- app.js
+++ app.js
@@ -52,6 +52,22 @@ app.get("/blogs",function(req, res){
     });
 });
 
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+
+app.post("/blogs",function(req, res){
+    blog.create(req.body.blog, function(err, Blog){
+        if(err){
+            console.log(err);
+            res.send("There occurred an Error");
+        }else{
+            console.log(Blog);
+            res.redirect("/");
+        }
+    });
+});
+
 
 
 
